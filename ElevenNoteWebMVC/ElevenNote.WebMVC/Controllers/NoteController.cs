ad8cd955aaee7833adb5798f80a4c9647bfb0589
atif <ab5143@hotmail.com>
2021-03-30 20:26:17 -0400

    First Round

---
+++ ElevenNoteWebMVC/ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,32 @@
+﻿using ElevenNoteMvc.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        ////Add method Here vvvv
+        ///GET
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ ElevenNoteWebMVC/ElevenNoteMvc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ElevenNoteMvc.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElevenNoteMvc.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c38bc08d-654b-4bda-b92a-fd86168beb9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

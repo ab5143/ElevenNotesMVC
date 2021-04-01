8df4c082be760271aee7354a1b39af42d23cfc32
atif <ab5143@hotmail.com>
2021-04-01 18:45:53 -0400

    Delete

---
+++ ElevenNoteWebMVC/ElevenNoteMvc.Model/NoteEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNoteMvc.Model
+{
+    public class NoteEdit
+    {
+
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+
+}
